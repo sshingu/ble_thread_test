21f76959a355fd18875e1efdbc2851dac7f364ef
神宮 将一 <sshingu@brycen.co.jp>
2019-04-19 16:43:08 +0900

    commit test

---
+++ examples/ble_central/ble_app_uart_c/pca10040/s132/ses/Output/ble_app_uart_c_pca10040_s132 Debug/Obj/ses_startup_nrf52.d
@@ -0,0 +1,2 @@
+Output/ble_app_uart_c_pca10040_s132\ Debug/Obj/ses_startup_nrf52.o: \
+ C:\nRF5_SDK_15.3.0_59ac345\modules\nrfx\mdk\ses_startup_nrf52.s
---
+++ examples/ble_central/ble_app_uart_c/pca10040/s132/ses/Output/ble_app_uart_c_pca10040_s132 Release/Obj/system_nrf52.d
@@ -0,0 +1,16 @@
+Output/ble_app_uart_c_pca10040_s132\ Release/Obj/system_nrf52.o: \
+ C:\nRF5_SDK_15.3.0_59ac345\modules\nrfx\mdk\system_nrf52.c \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.12/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.12/include/stdbool.h \
+ C:\nRF5_SDK_15.3.0_59ac345\modules\nrfx\mdk\nrf.h \
+ C:\nRF5_SDK_15.3.0_59ac345\modules\nrfx\mdk\nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ C:\nRF5_SDK_15.3.0_59ac345\modules\nrfx\mdk\system_nrf52.h \
+ C:\nRF5_SDK_15.3.0_59ac345\modules\nrfx\mdk\nrf52_bitfields.h \
+ C:\nRF5_SDK_15.3.0_59ac345\modules\nrfx\mdk\nrf51_to_nrf52.h \
+ C:\nRF5_SDK_15.3.0_59ac345\modules\nrfx\mdk\nrf52_name_change.h \
+ C:\nRF5_SDK_15.3.0_59ac345\modules\nrfx\mdk\compiler_abstraction.h
